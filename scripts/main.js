50be979d1223a75c3e6be6af89d93d00dbb49f73
juliamauroy <julia.mauroy@iths.se>
2020-11-05 15:08:54 +0100

    htmlcss

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
-console.log("hejhejhej")
+const links = document.querySelectorAll("nav > a")
+
+for(let link of links){
+    link.addEventListener("click", () =>{
+        document.querySelectorAll("main > section").forEach(
+            section => section.classList.remove("active")
+        )
+        
+        const section = document.querySelector(link.innertext.toLowerCase())
+        section.classList.add("active")
+    }
+    )
+}
